3da7b8004ea72bdf3a3d7a5f63114413f6e9ffed
zhangqi <zhangqihappyzq@163.com>
2021-05-17 14:02:43 +0800

    手动实现白色状态栏

--- app/src/main/java/com/example/statusbarstudy/MainActivity.java
+++ app/src/main/java/com/example/statusbarstudy/MainActivity.java
@@ -19,5 +19,10 @@ public class MainActivity extends AppCompatActivity {
 //                yourView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 //            }
 //        }
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !withoutUseStatusBarColor) {//android6.0以后可以对状态栏文字颜色和图标进行修改
+//            getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+//        }
+
     }
 }
